5340b4277f3c97b4afd6d8a107f70c141e847cfc
wushexu <wushexu@gmail.com>
2018-05-28 11:14:21 +0800

    sampleWords

--- models/word_category.js
+++ models/word_category.js
@@ -3,7 +3,7 @@ const {simpleCurd} = require('./db');
 let WordCategory = simpleCurd('word_categories');
 
 let requiredFields = ['code', 'name', 'dictCategoryKey', 'dictCategoryValue'],
-    updateFields = ['description', 'wordCount', 'extendTo', 'extendedWordCount', 'isFrequency', 'useAsUserBase'],
+    updateFields = ['description', 'wordCount', 'extendTo', 'extendedWordCount', 'isFrequency', 'useAsUserBase', 'no'],
     createFields = updateFields.concat(requiredFields);
 
 WordCategory.fields = {requiredFields, updateFields, createFields};
--- routes/admin/word_categories.js
+++ routes/admin/word_categories.js
@@ -1,12 +1,44 @@
 let express = require('express');
 let router = express.Router();
 
+let {wrapAsyncOne, getLimit} = require('../../common/helper');
 let restful = require('./common/rest');
+let Dict = require('../../models/dict');
 let WordCategory = require('../../models/word_category');
 
 
 let handles = restful.simpleHandles(WordCategory);
 restful.restful(router, handles);
 
+
+async function sampleWords(req, res, next) {
+    let code = req.params.code;
+    let wc = await WordCategory.coll().findOne({code},
+        {_id: 0, dictCategoryKey: 1, dictCategoryValue: 1});
+    if (!wc) {
+        res.json([]);
+    }
+
+    let filter = {['categories.' + wc.dictCategoryKey]: wc.dictCategoryValue};
+    let limit = getLimit(req, 20, 100);
+
+    let skip;
+    if (req.query.offset && !isNaN(req.query.offset)) {
+        skip = parseInt(req.query.offset);
+    } else {
+        let wordCount = wc.wordCount || 800;
+        skip = Math.floor((wordCount - limit) * Math.random());
+    }
+
+    let entries = await Dict.coll().find(filter,
+        {_id: 0, word: 1})
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+    let words = entries.map(e => e.word);
+    res.json(words);
+}
+
+router.post('/:code/sample', wrapAsyncOne(sampleWords));
 
 module.exports = router;
